16c1876821692aaa65f85abab0f11626a7f5bf64
Yarong Wang <wangyarong1266@gmail.com>
2023-09-14 10:30:03 +0800

    first version

--- CityImages/ViewController.swift
+++ CityImages/ViewController.swift
@@ -9,11 +9,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    
+    @IBAction func seattleButtonAction(_ sender: Any) {
+        imageView.image = UIImage(named: "seattle")
+    }
+    
+    @IBAction func laButtonAction(_ sender: Any) {
+        imageView.image = UIImage(named: "la")
+    }
+    @IBAction func austinButtonAction(_ sender: Any) {
+        imageView.image = UIImage(named: "austin")
+    }
+    @IBAction func nycButtonAction(_ sender: Any) {
+        imageView.image = UIImage(named: "nyc")
+    }
+    @IBAction func lasvegasButtonAction(_ sender: Any) {
+        imageView.image = UIImage(named: "lasvegas")
+    }
 }
 
